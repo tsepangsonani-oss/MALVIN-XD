970157d32875980824fc7cf1c167b0511c74ba1e
tsepangsonani-oss <tsepangsonani@gmail.com>
2025-08-18 01:26:42 +0200

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "",  //malvin~9nMCVaJL#x20mZPnOTHeIP4SaNsd8mtf8t_UYQCdxIOtlldmRG9E add your Session Id here
   
   OWNER_NUMBER: "263714757857", // put your phone number here
   
