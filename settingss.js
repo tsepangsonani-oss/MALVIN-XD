50d94b4515662bf335c5ad6cc0540590f4c67f1b
tsepangsonani-oss <tsepangsonani@gmail.com>
2025-08-18 01:40:37 +0200

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -2,7 +2,7 @@
 module.exports = {
   SESSION_ID: "",  //malvin~9nMCVaJL#x20mZPnOTHeIP4SaNsd8mtf8t_UYQCdxIOtlldmRG9E
   
-  OWNER_NUMBER: "263714757857", // +27764544823
+  OWNER_NUMBER: "263714757857", //27764544823
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
